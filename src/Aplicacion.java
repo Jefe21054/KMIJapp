d56f92b25a473de2a79a8b7b1c8ef232b7a35456
Jefe21054(Ivan) <ivaniglesias500@hotmail.com>
2020-03-09 22:16:03 -0500

    segundo commit

---
+++ src/Aplicacion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ivan
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+}
